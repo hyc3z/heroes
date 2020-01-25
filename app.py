4d186c08cd2d36731502f32895493d18c2189a53
Hycdog <tubao9hao@126.com>
2020-01-25 21:51:50 +0800

    Add sort.

--- app.py
+++ app.py
@@ -22,15 +22,18 @@ class AppMain(QDialog, Ui_app):
         self.show_heroes(self.hero_dict)
         self.listWidget_index.itemClicked.connect(self.show_detail)
         self.lineEdit_search.textChanged.connect(self.search)
+        self.pushButton_return.clicked.connect(self.return_index)
         self.stackedWidget.setCurrentIndex(0)
         self.init_table()
         self.show()
-
 
     def init_table(self):
         table = self.tableWidget_winrate
         table.setColumnCount(4)
         table.setHorizontalHeaderLabels(['英雄', '克制指数', '胜率', '盘数'])
+
+    def return_index(self):
+        self.stackedWidget.setCurrentIndex(0)
 
     def show_table(self, data:dict):
         table = self.tableWidget_winrate
@@ -39,21 +42,25 @@ class AppMain(QDialog, Ui_app):
         rowcount = 0
         for i in data:
             item0 = QTableWidgetItem(self.translation[i[0]])
+
             item0.setFlags(
                 QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             item0.setForeground(QBrush(QColor(204, 120, 50)))
             table.setItem(rowcount, 0, item0)
-            item1 = QTableWidgetItem(str(i[1][1]))
+            item1 = QTableWidgetItem()
+            item1.setData(QtCore.Qt.DisplayRole, float(i[1][1]))
             item1.setFlags(
                 QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             item1.setForeground(QBrush(QColor(85, 150, 186)))
             table.setItem(rowcount, 1, item1)
-            item2 = QTableWidgetItem(str(i[1][2]))
+            item2 = QTableWidgetItem()
+            item2.setData(QtCore.Qt.DisplayRole, float(i[1][2]))
             item2.setFlags(
                 QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             item2.setForeground(QBrush(QColor(106, 135, 89)))
             table.setItem(rowcount, 2, item2)
-            item3 = QTableWidgetItem(str(i[1][0]))
+            item3 = QTableWidgetItem()
+            item3.setData(QtCore.Qt.DisplayRole, float(i[1][0]))
             item3.setFlags(
                 QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             item3.setForeground(QBrush(QColor(106, 135, 89)))
@@ -82,6 +89,7 @@ class AppMain(QDialog, Ui_app):
         hero_name_english = self.translation[hero_name]
         winrate = get_hero_matchups_data_local_precise(hero_name_english)
         self.show_table(winrate)
+        self.label_currentHero.setText("当前英雄：{}".format(hero_name))
         self.stackedWidget.setCurrentIndex(1)
 
 
